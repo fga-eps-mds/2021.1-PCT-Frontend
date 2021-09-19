08ddeae3b5c55e7a3e21bdd5178eebd168641280
miguel.alves <miguel.alves@builtcode.com.br>
2021-09-19 15:59:34 -0300

    Issue #06 - Iniciando aplicaçao. Criando componente barra de pesquisa e primeira tela
    Co-authored-by: leticiards.04@gmail.com
    Co-authored-by: lucasgomesjk@gmail.com

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: #edffd3;
+        --webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button {
+        font: 16px sans-serif;
+    }
+
+    #root {
+        max-width: 1900px;
+        margin: 0 auto;
+        padding: 40px 20px;
+    }
+    
+`;
