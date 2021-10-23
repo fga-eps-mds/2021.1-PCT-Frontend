203baaeb4d73f0e8a8b6a07f63b82ac41cba8f94
Leticia <leticiards.04@gmail.com>
2021-10-23 16:30:02 -0300

    Rotas

--- src/Pages/AboutUs/index.tsx
+++ src/Pages/AboutUs/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Card } from "./styles";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
+
 
 const AboutUs: React.FC = () => {
   return (
     <Container>
       <Header />
+        <Card></Card>
       <Footer />
     </Container>
   );
--- src/Pages/AboutUs/styles.ts
+++ src/Pages/AboutUs/styles.ts
@@ -1,10 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  // height: 100vh;
+  height: 100vh;
   flex: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
 `;
+
+export const Card = styled.div`
+  background-color: white;
+  color: #272727;
+  padding: 20px;
+  border-radius: 10px;
+  border: 0px;
+  border-color: tomato;
+  border-style: solid;
+  transition: 200ms;
+  max-width: 380px;
+`; 
+
+
+
+
+	
