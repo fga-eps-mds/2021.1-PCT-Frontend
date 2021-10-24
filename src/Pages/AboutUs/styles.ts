a994ab0e334273ec91e29f112c41bc1fcb5aff77
Leticia <leticiards.04@gmail.com>
2021-10-24 12:14:11 -0300

    Contact correct

--- src/Pages/AboutUs/styles.ts
+++ src/Pages/AboutUs/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100vh;
+  height: 150vh;
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -17,27 +17,15 @@ export const CardHeader = styled.div`
 
 export const Card = styled.div`
   margin-top: 60px;
-  width: 300px;
+  width: 400px;
   border: none;
   border-radius: 15px;
-  background-color: #fff;
-  height: 300px;
+  height: 400px;
   margin-bottom: 300px;
   flex-direction: column;
-`;
-
-export const SubCard = styled.div`
-  width: 300px;
-  border: none;
-  border-radius: 15px;
-  background-color: #fff;
-  height: 300px;
-`;
-
-export const Image = styled.div`
-  max-width:100%;
-  max-height:100%;
-  margin-bottom: 50px;
+  flex: 1;
+  display: flex;
+  background-color: #172A3A;
 `;
 
 export const Title = styled.div`
@@ -46,6 +34,7 @@ export const Title = styled.div`
   font-size:20px;
   text-align: center;
   font-weight: bold;
+  font-size: 30px;
 `;
 
 export const TitleInfo = styled.div`
@@ -53,12 +42,71 @@ export const TitleInfo = styled.div`
   margin-bottom: 10px;
   text-align: justify;
   color: #1bc47d;
+  font-size: 18px;
 `;
+
 export const DescriptionInfo = styled.div`
   margin-top: 15px;
   text-align: justify;
   color: #1bc47d;
+  font-size: 15px;
 `;
+
+export const Image = styled.div`
+  border-radius: 60%;
+  width: 60px;
+  height: 60px;
+  margin-left: 40px;
+  margin-top: 20px;
+  margin-right: 20px;
+`;
+
+export const ContactsInfo = styled.div`
+  text-align: justify;
+  color: #FFFFFF;
+  font-size: 20px;
+`;
+
+export const Contacts = styled.div`
+  max-width:100%;
+  max-height:100%;
+  margin-bottom: 20px;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  display: flex;
+`;
+
+export const TeamCard = styled.div`
+  max-width:100%;
+  max-height:100%;
+  margin-bottom: 20px;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  display: flex;
+  
+`;
+
+export const Dev = styled.div`
+  max-width:100%;
+  max-height:100%;
+  margin-bottom: 20px;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  display: flex;
+`;
+
+export const DevInfo = styled.div`
+  text-align: justify;
+  color: #1bc47d;
+  font-size: 20px;
+`;
+
+
+
+
 
 
 
